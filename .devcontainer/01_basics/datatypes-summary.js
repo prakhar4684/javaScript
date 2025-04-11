5f54f3321a81a0f26e20e1c0ffce2800a8e56005
prakhar4684 <praakharshukla4004@gmail.com>
2025-04-11 13:10:12 +0000

    change

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -22,3 +22,20 @@ const myfunction= function(){
     console.log("hello guys")
 }
 console.log(typeof myfunction)
+//+++++++++++++++++++++++++++++++++++++///
+//#premitive data types always use STACK (to asign copy value to another variable)
+//#Non-premitive data types always use HEAP (to asign refrence of a value to another value)
+//ex-of premitive datatypes
+let myname="prakhar"
+let yourname=myname
+yourname="adarsh"
+console.log(yourname,myname)// here we can see copying value to another variable 
+//ex-of Non-premitive datatypes
+let obj1={
+    naam:"prakhar",
+    work:"student",
+    age:20
+}
+let obj2=obj1;
+obj12.work="software engineer"
+//here obj1,obj2 point to the same contianer of object that creat in memory i.e both have same refrence.  
