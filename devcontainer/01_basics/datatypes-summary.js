d07089dad055c217a6c67d1ab88efe23009ce702
prakhar4684 <praakharshukla4004@gmail.com>
2025-04-11 12:19:40 +0000

    data types summary

---
+++ devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,24 @@
+//premitive
+//7 Types:String ,Number,Booliean,Symbol,Bigint,null,undefined 
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp =null
+let userEmail=undefined;
+const id=symbol('123')
+const anotherId=symbol('123')
+console.log(id===anotherId)
+const bigInt=9483978938399270932749233984
+//Refrence type
+//3 types: Array,objects,functions
+
+const heros=["shaktiman","krish","doga"]
+let myobj={
+    name:"prakhar",
+    age:20,
+    wt:55
+}
+const myfunction= function(){
+    console.log("hello guys")
+}
+console.log(typeof myfunction)
