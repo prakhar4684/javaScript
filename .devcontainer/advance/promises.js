7a25639a31f1fbc89885f2cb452e341c66b6605c
prakhar4684 <praakharshukla4004@gmail.com>
2025-04-21 17:29:06 +0000

    promises

---
+++ .devcontainer/advance/promises.js
@@ -0,0 +1,24 @@
+//Promises
+//promise is for "eventual" compition of task . it is an object in js .
+//it is a solution of callback
+// syntax
+//let promise =new Promise((resolve,reject)=>{
+//    console.log("Iam a promise")// if we write only that then the state of promise is pending.
+//    //resolve("prakhar")
+//    reject("something went wrong");
+//})
+function  getData(dataId,getNextData){
+    return new promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("data",dataId)
+            resolve("success")
+        if(getNextData)getNextData();
+        },2000)
+    })
+}
+getData(10);
+getData(20);
+// a java script Promise object can be:
+//pending:the result is undefined
+//resolve: the result is a value (fulfilled)
+//rejected: the result is n erroe object
